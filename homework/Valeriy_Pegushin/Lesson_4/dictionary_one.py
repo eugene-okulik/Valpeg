7d1dec6a288004bdd959f7828044d1c3b9192778
Валерий Пегушин <valpegushin@yandex.ru>
2025-05-22 15:47:22 +0500

    создание коллекции

---
+++ homework/Valeriy_Pegushin/Lesson_4/dictionary_one.py
@@ -0,0 +1,13 @@
+my_dict = {'tuple' : (1, 'two', 3, 4.12, False),
+           'list' : [1, 'two', 3, 4.12, False],
+           'dict' : {'one' : 1, 'two' : 'two', 'three' : 3, 'four' : 4.12, 'five' : False},
+           'set' : {1, 'two', 3, 4.12, False}}
+print(my_dict['tuple'][-1])
+my_dict['list'].append(True)
+my_dict['list'].pop(1)
+my_dict['dict']['i am a tuple'] = 'any value'
+my_dict['dict'].pop('one')
+my_dict['set'].add(6)
+my_dict['set'].pop()
+
+print(my_dict)
