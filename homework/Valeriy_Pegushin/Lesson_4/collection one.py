1dbe67959b39a455c68a3f39e62f7a58a8089d66
Валерий Пегушин <valpegushin@yandex.ru>
2025-05-24 11:21:35 +0500

    добавление элемента с ключом ('i am a tuple',)

--- homework/Valeriy_Pegushin/Lesson_4/collection one.py
+++ homework/Valeriy_Pegushin/Lesson_4/collection one.py
@@ -5,7 +5,7 @@ my_dict = {'tuple': (1, 'two', 3, 4.12, False),
 print(my_dict['tuple'][-1])
 my_dict['list'].append(True)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 'any value'
+my_dict['dict'][('i am a tuple',)] = 'any value'
 my_dict['dict'].pop('one')
 my_dict['set'].add(6)
 my_dict['set'].pop()
