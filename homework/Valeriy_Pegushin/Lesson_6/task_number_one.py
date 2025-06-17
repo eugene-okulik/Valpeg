43fb8bea169d54576118faefac26f29aea1349b8
Валерий Пегушин <valpegushin@yandex.ru>
2025-06-17 13:56:58 +0500

    Условия и циклы в Python

--- homework/Valeriy_Pegushin/Lesson_6/task_number_one.py
+++ homework/Valeriy_Pegushin/Lesson_6/task_number_one.py
@@ -1,5 +1,4 @@
-text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
-        " Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 
 # Преобразуем строку в список
 words = text.split()
