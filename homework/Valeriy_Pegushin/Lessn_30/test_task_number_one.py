709844d49f14b60a5066a099b34a8e1293ccd642
Валерий Пегушин <valpegushin@yandex.ru>
2026-04-22 09:43:43 +0500

    Playwright - работа с запросами

--- homework/Valeriy_Pegushin/Lessn_30/test_task_number_one.py
+++ homework/Valeriy_Pegushin/Lessn_30/test_task_number_one.py
@@ -1,19 +1,9 @@
-from playwright.sync_api import Page, expect, Request, Response
-import re
+from playwright.sync_api import Page, expect
 
 
 def test_iphone_17_pro_modal_with_mocked_name(page: Page):
-    def handle_route(route):
-        response = route.fetch()
-        body = response.json()
-        body['body']['digitalMat'][0]['productName'] = "яблокофон 17 про"
-        route.fulfill(
-            status=response.status,
-            headers=response.headers,
-            json=body
-        )
-    page.route(re.compile('digital-mat'), handle_route)
     page.goto('https://www.apple.com/shop/buy-iphone')
-    page.locator('.rf-hcard-copy').locator('nth=0').click()
-    modal_header = page.locator('#rf-digitalmat-overlay-label-0').locator('nth=0')
-    expect(modal_header).to_contain_text("яблокофон 17 про")
+    page.locator('.rf-hcard-copy').first.click()
+    page.wait_for_selector('#rf-digitalmat-overlay-label-0', state='visible')
+    page.evaluate('document.querySelector("#rf-digitalmat-overlay-label-0").innerText = "яблокофон 17 про"')
+    expect(page.locator('#rf-digitalmat-overlay-label-0').first).to_contain_text("яблокофон 17 про")
